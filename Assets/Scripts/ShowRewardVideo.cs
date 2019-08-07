c3589c92d088aaed2f26cba0867b505b627ef446
Vlad Kravchenko <vladkravchenko@Vlads-MacBook-Pro.local>
2019-08-07 13:01:44 +0200

    Show video done

--- Assets/Scripts/ShowRewardVideo.cs
+++ Assets/Scripts/ShowRewardVideo.cs
@@ -7,8 +7,6 @@ public class ShowRewardVideo : MonoBehaviour
     GameObject InitText;
     GameObject ShowButton;
     GameObject ShowText;
-    GameObject AmountText;
-    int userTotalCredits = 0;
 
     public static String REWARDED_INSTANCE_ID = "0";
 
@@ -21,10 +19,8 @@ public class ShowRewardVideo : MonoBehaviour
         ShowText = GameObject.Find("Text");
         ShowText.GetComponent<UnityEngine.UI.Text>().color = UnityEngine.Color.red;
 
-        AmountText = GameObject.Find("RVAmount");
-
-        //Add Rewarded Video Events
-        IronSourceEvents.onRewardedVideoAdOpenedEvent += RewardedVideoAdOpenedEvent;
+		//Add Rewarded Video Events
+		IronSourceEvents.onRewardedVideoAdOpenedEvent += RewardedVideoAdOpenedEvent;
         IronSourceEvents.onRewardedVideoAdClosedEvent += RewardedVideoAdClosedEvent;
         IronSourceEvents.onRewardedVideoAvailabilityChangedEvent += RewardedVideoAvailabilityChangedEvent;
         IronSourceEvents.onRewardedVideoAdStartedEvent += RewardedVideoAdStartedEvent;
@@ -105,14 +101,12 @@ public class ShowRewardVideo : MonoBehaviour
         Debug.Log("unity-script: I got RewardedVideoAdOpenedEvent");
     }
 
-    void RewardedVideoAdRewardedEvent(IronSourcePlacement ssp)
-    {
-        Debug.Log("unity-script: I got RewardedVideoAdRewardedEvent, amount = " + ssp.getRewardAmount() + " name = " + ssp.getRewardName());
-        userTotalCredits = userTotalCredits + ssp.getRewardAmount();
-        AmountText.GetComponent<UnityEngine.UI.Text>().text = "" + userTotalCredits;
-    }
+	void RewardedVideoAdRewardedEvent(IronSourcePlacement ssp)
+	{
+		Debug.Log("unity-script: I got RewardedVideoAdRewardedEvent, amount = " + ssp.getRewardAmount() + " name = " + ssp.getRewardName());
+	}
 
-    void RewardedVideoAdClosedEvent()
+	void RewardedVideoAdClosedEvent()
     {
         Debug.Log("unity-script: I got RewardedVideoAdClosedEvent");
     }
