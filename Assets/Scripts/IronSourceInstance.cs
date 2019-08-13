37ab5b0cb950245c994cbe4884043b2d9c03dee1
Vlad Kravchenko <vladkravchenko@Vlads-MacBook-Pro.local>
2019-08-13 15:59:57 +0200

    Added IronSourceInstance singleton

---
+++ Assets/Scripts/IronSourceInstance.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronSourceInstance
+{
+	private static IronSourceInstance _Instance;
+
+    private static string UNIQUE_USER_ID = "demoUserUnity";
+    private static string APP_KEY = "9c530d5d";
+    private static string REWARDED_INSTANCE_ID = "0";
+
+	public static IronSource Instance()
+	{
+		if (_Instance == null)
+		{
+            _Instance = new IronSourceInstance();
+            GameObject myRoadInstance = GameObject.Instantiate(Resources.Load("IronSourceEventsPrefab")) as GameObject;
+            //Dynamic config example
+            IronSourceConfig.Instance.setClientSideCallbacks(true);
+			string id = IronSource.Agent.getAdvertiserId();
+			IronSource.Agent.validateIntegration();
+			IronSource.Agent.setUserId(UNIQUE_USER_ID);
+			IronSource.Agent.init(APP_KEY, IronSourceAdUnits.REWARDED_VIDEO);
+			IronSource.Agent.validateIntegration();
+
+			// SDK init
+			IronSource.Agent.init(APP_KEY);
+
+			//Add Rewarded Video Events
+			IronSourceEvents.onRewardedVideoAdOpenedEvent += RewardedVideoAdOpenedEvent;
+			IronSourceEvents.onRewardedVideoAdClosedEvent += RewardedVideoAdClosedEvent;
+			IronSourceEvents.onRewardedVideoAdStartedEvent += RewardedVideoAdStartedEvent;
+			IronSourceEvents.onRewardedVideoAdEndedEvent += RewardedVideoAdEndedEvent;
+			IronSourceEvents.onRewardedVideoAdRewardedEvent += RewardedVideoAdRewardedEvent;
+			IronSourceEvents.onRewardedVideoAdShowFailedEvent += RewardedVideoAdShowFailedEvent;
+			IronSourceEvents.onRewardedVideoAdClickedEvent += RewardedVideoAdClickedEvent;
+		}
+
+		return IronSource.Agent;
+
+	}
+
+	private void OnDestroy()
+	{
+		if (_Instance == this)
+		{
+			_Instance = null;
+		}
+	}
+
+    void OnApplicationPause(bool isPaused)
+    {
+        Debug.Log("unity-script: OnApplicationPause = " + isPaused);
+        IronSource.Agent.onApplicationPause(isPaused);
+    }
+
+    /************* RewardedVideo API *************/
+    void ShowRewardedVideoButtonClicked()
+	{
+		Debug.Log("unity-script: ShowRewardedVideoButtonClicked");
+		if (IronSource.Agent.isRewardedVideoAvailable())
+		{
+			IronSource.Agent.showRewardedVideo();
+		}
+		else
+		{
+			Debug.Log("unity-script: IronSource.Agent.isRewardedVideoAvailable - False");
+		}
+	}
+
+	void LoadDemandOnlyRewardedVideo()
+	{
+		Debug.Log("unity-script: LoadDemandOnlyRewardedVideoButtonClicked");
+		IronSource.Agent.loadISDemandOnlyRewardedVideo(REWARDED_INSTANCE_ID);
+	}
+
+	void ShowDemandOnlyRewardedVideo()
+	{
+		Debug.Log("unity-script: ShowDemandOnlyRewardedVideoButtonClicked");
+		if (IronSource.Agent.isISDemandOnlyRewardedVideoAvailable(REWARDED_INSTANCE_ID))
+		{
+			IronSource.Agent.showISDemandOnlyRewardedVideo(REWARDED_INSTANCE_ID);
+		}
+		else
+		{
+			Debug.Log("unity-script: IronSource.Agent.isISDemandOnlyRewardedVideoAvailable - False");
+		}
+	}
+
+    /************* RewardedVideo Delegates *************/
+	static void RewardedVideoAdOpenedEvent()
+	{
+		Debug.Log("unity-script: I got RewardedVideoAdOpenedEvent");
+	}
+
+    static void RewardedVideoAdRewardedEvent(IronSourcePlacement ssp)
+	{
+		Debug.Log("unity-script: I got RewardedVideoAdRewardedEvent, amount = " + ssp.getRewardAmount() + " name = " + ssp.getRewardName());
+	}
+
+    static void RewardedVideoAdClosedEvent()
+	{
+		Debug.Log("unity-script: I got RewardedVideoAdClosedEvent");
+	}
+
+    static void RewardedVideoAdStartedEvent()
+	{
+		Debug.Log("unity-script: I got RewardedVideoAdStartedEvent");
+	}
+
+    static void RewardedVideoAdEndedEvent()
+	{
+		Debug.Log("unity-script: I got RewardedVideoAdEndedEvent");
+	}
+
+    static void RewardedVideoAdShowFailedEvent(IronSourceError error)
+	{
+		Debug.Log("unity-script: I got RewardedVideoAdShowFailedEvent, code :  " + error.getCode() + ", description : " + error.getDescription());
+	}
+
+    static void RewardedVideoAdClickedEvent(IronSourcePlacement ssp)
+	{
+		Debug.Log("unity-script: I got RewardedVideoAdClickedEvent, name = " + ssp.getRewardName());
+	}
+}
--- Assets/Scripts/RewardAdvertButton.cs
+++ Assets/Scripts/RewardAdvertButton.cs
@@ -14,7 +14,7 @@ namespace Assets.Scripts
 
 		public static string UNIQUE_USER_ID = "demoUserUnity";
 		public static string APP_KEY = "9c530d5d";
-		public static String REWARDED_INSTANCE_ID = "0";
+		public static string REWARDED_INSTANCE_ID = "0";
 
 		float AdTimeOut = 3.0f;
 
@@ -34,43 +34,18 @@ namespace Assets.Scripts
 
         public Button Btn => button;
 
-        void Start()
-		{
-			Debug.Log("unity-script: RewardedVideoButton Start called");
-
-			//Dynamic config example
-			IronSourceConfig.Instance.setClientSideCallbacks(true);
-			string id = IronSource.Agent.getAdvertiserId();
-			Debug.Log("unity-script: IronSource.Agent.getAdvertiserId : " + id);
-			IronSource.Agent.validateIntegration();
-			Debug.Log("unity-script: unity version" + IronSource.unityVersion());
-			IronSource.Agent.setUserId(UNIQUE_USER_ID);
-			IronSource.Agent.init(APP_KEY, IronSourceAdUnits.REWARDED_VIDEO);
-			IronSource.Agent.validateIntegration();
-
-			// SDK init
-			IronSource.Agent.init(APP_KEY);
-
-			//Add Rewarded Video Events
-			IronSourceEvents.onRewardedVideoAdOpenedEvent += RewardedVideoAdOpenedEvent;
-			IronSourceEvents.onRewardedVideoAdClosedEvent += RewardedVideoAdClosedEvent;
-			IronSourceEvents.onRewardedVideoAvailabilityChangedEvent += RewardedVideoAvailabilityChangedEvent;
-			IronSourceEvents.onRewardedVideoAdStartedEvent += RewardedVideoAdStartedEvent;
-			IronSourceEvents.onRewardedVideoAdEndedEvent += RewardedVideoAdEndedEvent;
-			IronSourceEvents.onRewardedVideoAdRewardedEvent += RewardedVideoAdRewardedEvent;
-			IronSourceEvents.onRewardedVideoAdShowFailedEvent += RewardedVideoAdShowFailedEvent;
-			IronSourceEvents.onRewardedVideoAdClickedEvent += RewardedVideoAdClickedEvent;
-		}
-
         void Awake()
         {
-			ShowText = GameObject.Find("Text");
+            Debug.Log("Instantiating IronSource prefab");
+            IronSourceInstance.Instance();
+            ShowText = GameObject.Find("Text");
             if (button == null)
                 button = GetComponent<Button>();
             button.onClick.AddListener(ShowAd);
             OnShownAdd += _GetReward;
 			ShowText.GetComponent<UnityEngine.UI.Text>().color = UnityEngine.Color.red;
-			OnAwake();
+            IronSourceEvents.onRewardedVideoAvailabilityChangedEvent += RewardedVideoAvailabilityChangedEvent;
+            OnAwake();
         }
 
         protected virtual void OnAwake()
@@ -104,9 +79,9 @@ namespace Assets.Scripts
 
             if (!_lock)
             {
-                _lock = true;
+				_lock = true;
                 Debug.Log("SHOW AD");
-                StartCoroutine(WaitTimer(IronSource.Agent.isRewardedVideoAvailable));
+                StartCoroutine(WaitTimer(IronSourceInstance.Instance().isRewardedVideoAvailable));
             }
             else
             {
@@ -140,7 +115,7 @@ namespace Assets.Scripts
             {
                 OnAdFound.Invoke();
                 Debug.Log("WaitTimer " + Thread.CurrentThread.ManagedThreadId);
-                IronSource.Agent.showRewardedVideo();
+                IronSourceInstance.Instance().showRewardedVideo();
             }
             else
             {
@@ -183,103 +158,27 @@ namespace Assets.Scripts
 
         }
 
+        void RewardedVideoAvailabilityChangedEvent(bool canShowAd)
+        {
+            Debug.Log("unity-script: I got RewardedVideoAvailabilityChangedEvent, value = " + canShowAd);
+            if (canShowAd)
+            {
+                Debug.Log("BLUE!");
+                ShowText.GetComponent<UnityEngine.UI.Text>().color = UnityEngine.Color.blue;
+            }
+            else
+            {
+                Debug.Log("RED!");
+                ShowText.GetComponent<UnityEngine.UI.Text>().color = UnityEngine.Color.red;
+            }
+        }
+
         public void Test(bool isShsowned)
         {
             Time.timeScale = 1.0f;
         }
 
         protected virtual void GetReward(bool isShowned) { }
-
-		/************* RewardedVideo API *************/
-		public void ShowRewardedVideoButtonClicked()
-		{
-			Debug.Log("unity-script: ShowRewardedVideoButtonClicked");
-			if (IronSource.Agent.isRewardedVideoAvailable())
-			{
-				IronSource.Agent.showRewardedVideo();
-			}
-			else
-			{
-				Debug.Log("unity-script: IronSource.Agent.isRewardedVideoAvailable - False");
-			}
-
-			// DemandOnly
-			// ShowDemandOnlyRewardedVideo ();
-		}
-
-		void LoadDemandOnlyRewardedVideo()
-		{
-			Debug.Log("unity-script: LoadDemandOnlyRewardedVideoButtonClicked");
-			IronSource.Agent.loadISDemandOnlyRewardedVideo(REWARDED_INSTANCE_ID);
-		}
-
-		void ShowDemandOnlyRewardedVideo()
-		{
-			Debug.Log("unity-script: ShowDemandOnlyRewardedVideoButtonClicked");
-			if (IronSource.Agent.isISDemandOnlyRewardedVideoAvailable(REWARDED_INSTANCE_ID))
-			{
-				IronSource.Agent.showISDemandOnlyRewardedVideo(REWARDED_INSTANCE_ID);
-			}
-			else
-			{
-				Debug.Log("unity-script: IronSource.Agent.isISDemandOnlyRewardedVideoAvailable - False");
-			}
-		}
-
-		/************* RewardedVideo Delegates *************/
-		void RewardedVideoAvailabilityChangedEvent(bool canShowAd)
-		{
-			Debug.Log("unity-script: I got RewardedVideoAvailabilityChangedEvent, value = " + canShowAd);
-			if (canShowAd)
-			{
-				ShowText.GetComponent<UnityEngine.UI.Text>().color = UnityEngine.Color.blue;
-			}
-			else
-			{
-				ShowText.GetComponent<UnityEngine.UI.Text>().color = UnityEngine.Color.red;
-			}
-		}
-
-		void RewardedVideoAdOpenedEvent()
-		{
-			Debug.Log("unity-script: I got RewardedVideoAdOpenedEvent");
-		}
-
-		void RewardedVideoAdRewardedEvent(IronSourcePlacement ssp)
-		{
-			Debug.Log("unity-script: I got RewardedVideoAdRewardedEvent, amount = " + ssp.getRewardAmount() + " name = " + ssp.getRewardName());
-		}
-
-		void RewardedVideoAdClosedEvent()
-		{
-			Debug.Log("unity-script: I got RewardedVideoAdClosedEvent");
-		}
-
-		void RewardedVideoAdStartedEvent()
-		{
-			Debug.Log("unity-script: I got RewardedVideoAdStartedEvent");
-		}
-
-		void RewardedVideoAdEndedEvent()
-		{
-			Debug.Log("unity-script: I got RewardedVideoAdEndedEvent");
-		}
-
-		void RewardedVideoAdShowFailedEvent(IronSourceError error)
-		{
-			Debug.Log("unity-script: I got RewardedVideoAdShowFailedEvent, code :  " + error.getCode() + ", description : " + error.getDescription());
-		}
-
-		void RewardedVideoAdClickedEvent(IronSourcePlacement ssp)
-		{
-			Debug.Log("unity-script: I got RewardedVideoAdClickedEvent, name = " + ssp.getRewardName());
-		}
-
-		void OnApplicationPause(bool isPaused)
-		{
-			Debug.Log("unity-script: OnApplicationPause = " + isPaused);
-			IronSource.Agent.onApplicationPause(isPaused);
-		}
 
 #if UNITY_EDITOR
         private void Reset()
